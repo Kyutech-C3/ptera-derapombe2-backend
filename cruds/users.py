008c388160504a417b0c6c9be54b8a3e0c65fe10
PigeonsHouse <bullbull1230@gmail.com>
2022-09-18 15:23:30 +0900

    [update] エラーハンドリングの修正

--- cruds/users.py
+++ cruds/users.py
@@ -1,18 +1,15 @@
 from db.models import User
 from sqlalchemy.orm.session import Session
-from fastapi import HTTPException
 from schemas.users import ColorType
 
 def get_user_by_id(db: Session, user_id: str) -> User:
 	user = db.query(User).get(user_id)
 	if user is None:
-		raise HTTPException(status_code=404, detail='user is not found')
+		raise Exception('user is not found')
 	return user
 
 def get_users(db: Session) -> list[User]:
 	users = db.query(User).all()
-	if users is None:
-		raise HTTPException(status_code=404, detail='user is not found')
 	return users
 
 def create_user(db: Session, name: str, group: ColorType, avatar_number: int) -> User:
