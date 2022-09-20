5a34a284e79346072a8b026438e4c5d44e23f832
PigeonsHouse <bullbull1230@gmail.com>
2022-09-20 14:30:20 +0900

    [delete] get_user

--- routers/users.py
+++ routers/users.py
@@ -5,11 +5,6 @@ from routers.utils import verify_token
 from schemas.users import ColorType, UserType
 from strawberry.types import Info
 
-def get_user(user_id: str) -> UserType:
-	db = next(get_db())
-	user = cu.get_user_by_id(db, user_id)
-	return UserType.from_instance(user)
-
 def get_me(info: Info) -> UserType:
 	db = next(get_db())
 	user_id = verify_token(info)
