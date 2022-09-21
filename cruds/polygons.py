4b023960997c3ec6aa8079fbdc07856e3dc13a1d
PigeonsHouse <bullbull1230@gmail.com>
2022-09-22 05:00:57 +0900

    [update] mockに切り戻し

--- cruds/polygons.py
+++ cruds/polygons.py
@@ -1,14 +1,18 @@
+from datetime import datetime
 from sqlalchemy import func, or_, and_
 from sqlalchemy.orm import Session
-from db.models import LinkingSign, Polygon, Sign, Color, SignStatus
+from db.models import BaseSign, Item, LinkingSign, Polygon, Sign, Color, SignStatus
 from cruds.users import get_user_by_id
 from cruds.signs import get_sign_by_id
+from schemas.general import ColorType
 from schemas.polygons import Link, MapInfo, PolygonType, PowerRatio
 from schemas.signs import Coordinate, SignType
 from shapely.geometry import shape
 from shapely.ops import transform
 import pyproj
 from functools import partial
+
+from schemas.users import UserType
 
 LENGTH_KILO_MATER = 10
 LENGTH_DEGREE = LENGTH_KILO_MATER * 90 / 10000
@@ -183,16 +187,215 @@ def swap_xy(geom):
         raise TypeError('Unexpected geom.type:', geom.type)
 
 def get_map_info(db: Session):
-	map_signs = []
-	signs = db.query(Sign).all()
-	for sign in signs:
-		sign_status = db.query(SignStatus).filter(SignStatus.sign_id == sign.id).first()
-		map_signs.append(SignType.from_instance(sign, sign_status))
-
-	linking = db.query(LinkingSign).all()
-	polygons = db.query(Polygon).all()
+	items = db.query(Item).all()
+	base_signs = db.query(BaseSign).all()
 	return MapInfo(
-		signs=map_signs,
-		links=[Link.from_instance(link) for link in linking],
-		polygons=[PolygonType.from_instance(polygon) for polygon in polygons]
+		signs=[
+			SignType(
+				link_num=2,
+				id='11111111111',
+				base_signs=[
+					base_signs[22]
+				],
+				coordinate=Coordinate(
+					latitude=33.88236993479559,
+					longitude=130.87800726001615,
+				),
+				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+				max_hit_point=100,
+				max_item_slot=6,
+				max_link_slot=2,
+				owner=UserType(
+					id='12345',
+					name='name',
+					group=ColorType.RED,
+					exp_point=0,
+					avatar_url='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+					level=1,
+					created_at=datetime.now(),
+					updated_at=datetime.now()
+				),
+				created_at=datetime.now(),
+				group=ColorType.RED, hit_point=24,
+				items=[
+					items[0],
+					items[1],
+					items[0],
+				]
+			),
+			SignType(
+				link_num=2,
+				id='2222222222',
+				base_signs=[
+					base_signs[22]
+				],
+				coordinate=Coordinate(
+					latitude=33.88240831993862,
+					longitude=130.8778279509555,
+				),
+				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+				max_hit_point=100,
+				max_item_slot=6,
+				max_link_slot=2,
+				created_at=datetime.now(),
+				group=ColorType.RED, hit_point=24, items=[
+					items[0],
+					items[3],
+					items[5],
+				],
+				owner=UserType(
+					id='12345',
+					name='name',
+					group=ColorType.RED,
+					avatar_url='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+					exp_point=0,
+					level=1,
+					created_at=datetime.now(),
+					updated_at=datetime.now()
+				),
+			),
+			SignType(
+				link_num=2,
+				id='3333333333333333',
+				base_signs=[
+					base_signs[22]
+				],
+				owner=UserType(
+					id='12345',
+					name='name',
+					group=ColorType.RED,
+					exp_point=0,
+					avatar_url='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+					level=1,
+					created_at=datetime.now(),
+					updated_at=datetime.now()
+				),
+				coordinate=Coordinate(
+					latitude=33.88223979967025,
+					longitude=130.87878426594528,
+				),
+				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+				max_hit_point=100,
+				max_item_slot=6,
+				max_link_slot=2,
+				created_at=datetime.now(),
+				group=ColorType.RED, hit_point=24, items=[
+					items[0],
+					items[2],
+				]
+			),
+			SignType(
+				link_num=0,
+				id='3333333333333344',
+				base_signs=[
+					base_signs[22]
+				],
+				coordinate=Coordinate(
+					latitude=33.88204880963038,
+					longitude=130.8784008377654,
+				),
+				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+				max_hit_point=100,
+				owner=UserType(
+					id='12345',
+					name='name',
+					group=ColorType.GREEN,
+					exp_point=0,
+					avatar_url='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+					level=1,
+					created_at=datetime.now(),
+					updated_at=datetime.now()
+				),
+				max_item_slot=6,
+				max_link_slot=2,
+				created_at=datetime.now(),
+				group=ColorType.GREEN, hit_point=24, items=[
+					items[0],
+					items[1],
+				]
+			),
+		],
+		links=[
+			Link(
+				polygon_id='88888888',
+				group=ColorType.RED,
+				sign_id='11111111111',
+				other_sign_id='2222222222',
+				one_coordinate=Coordinate(
+					latitude=33.88236993479559,
+					longitude=130.87800726001615,
+				),
+				other_coordinate=Coordinate(
+					latitude=33.88240831993862,
+					longitude=130.8778279509555,
+				),
+				created_at=datetime.now()
+			),
+			Link(
+				polygon_id='88888888',
+				group=ColorType.RED,
+				sign_id='2222222222',
+				other_sign_id='3333333333333333',
+				one_coordinate=Coordinate(
+					latitude=33.88240831993862,
+					longitude=130.8778279509555,
+				),
+				other_coordinate=Coordinate(
+					latitude=33.88223979967025,
+					longitude=130.87878426594528,
+				),
+				created_at=datetime.now()
+			),
+			Link(
+				polygon_id='88888888',
+				group=ColorType.RED,
+				sign_id='11111111111',
+				other_sign_id='3333333333333333',
+				one_coordinate=Coordinate(
+					latitude=33.88236993479559,
+					longitude=130.87800726001615,
+				),
+				other_coordinate=Coordinate(
+					latitude=33.88223979967025,
+					longitude=130.87878426594528,
+				),
+				created_at=datetime.now()
+			)
+		],
+		polygons=[
+			PolygonType(
+				id='88888888',
+				group=ColorType.RED,
+				surface=1.2456,
+				sign_ids=['11111111111', '2222222222', '3333333333333333'],
+				coordinates=[
+					Coordinate(
+						latitude=33.88236993479559,
+						longitude=130.87800726001615,
+					),
+					Coordinate(
+						latitude=33.88240831993862,
+						longitude=130.8778279509555,
+					),
+					Coordinate(
+						latitude=33.88223979967025,
+						longitude=130.87878426594528,
+					),
+				],
+				created_at=datetime.now()
+			)
+		]
 	)
+	# map_signs = []
+	# signs = db.query(Sign).all()
+	# for sign in signs:
+	# 	sign_status = db.query(SignStatus).filter(SignStatus.sign_id == sign.id).first()
+	# 	map_signs.append(SignType.from_instance(sign, sign_status))
+
+	# linking = db.query(LinkingSign).all()
+	# polygons = db.query(Polygon).all()
+	# return MapInfo(
+	# 	signs=map_signs,
+	# 	links=[Link.from_instance(link) for link in linking],
+	# 	polygons=[PolygonType.from_instance(polygon) for polygon in polygons]
+	# )
