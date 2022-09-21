c34f8459f24253523efd2eb3c328c3c507090311
PigeonsHouse <bullbull1230@gmail.com>
2022-09-21 22:20:32 +0900

    [fix] int

--- routers/leveling.py
+++ routers/leveling.py
@@ -5,9 +5,9 @@ from db.database import get_db
 from routers.utils import verify_token
 from schemas.leveling import Leveling
 
-def get_level(level: str, info: Info) -> Leveling:
+def get_level(level: int, info: Info) -> Leveling:
 	db = next(get_db())
-	user_id = verify_token(info)
+	verify_token(info)
 
 	leveling = cl.get_leveling(db, level)
 
