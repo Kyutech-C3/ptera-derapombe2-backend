b3d6bf8bd96cf60739e6b64ed41f29dc880490ee
PigeonsHouse <65777862+PigeonsHouse@users.noreply.github.com>
2022-09-21 20:37:32 +0900

    Update polygons.py

--- cruds/polygons.py
+++ cruds/polygons.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from db.models import Color, Polygon
 from schemas.polygons import PowerRatio
 
-def calc_ratio(db: Session):
+def calc_power_ratio(db: Session):
 	red_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.RED).group_by(Polygon.group)
 	green_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.GREEN).group_by(Polygon.group)
 
