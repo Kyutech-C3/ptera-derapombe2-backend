2dca5d94a21c56bfb3e10a8f331ecc383ee5ab08
PigeonsHouse <bullbull1230@gmail.com>
2022-09-21 17:54:59 +0900

    [update] モックの更新

--- routers/polygons.py
+++ routers/polygons.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from schemas.general import ColorType
 from schemas.polygons import MapInfo, PolygonType, PowerRatio, Segment
 from schemas.signs import SignType
+from schemas.users import UserType
 
 def get_power_ratio() -> PowerRatio:
 	return PowerRatio(
@@ -15,20 +16,59 @@ def get_map_info() -> MapInfo:
 			SignType(
 				id='11111111111',
 				base_sign_types=[1],
-				longitude=130.671892,
-				latitude=33.654921,
+				latitude=33.88236993479559,
+				longitude=130.87800726001615,
 				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
 				max_hit_point=100,
 				max_item_slot=8,
 				max_link_slot=12,
+				owner=UserType(
+					id='12345',
+					name='name',
+					group=ColorType.RED,
+					exp_point=0,
+					level=1,
+					created_at=datetime.now(),
+					updated_at=datetime.now()
+				),
 				created_at=datetime.now(),
 				group=ColorType.RED, hit_point=24, items=[]
 			),
 			SignType(
 				id='2222222222',
 				base_sign_types=[1],
-				longitude=130.671892,
-				latitude=33.654921,
+				latitude=33.88240831993862,
+				longitude=130.8778279509555,
+				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
+				max_hit_point=100,
+				max_item_slot=8,
+				max_link_slot=12,
+				created_at=datetime.now(),
+				group=ColorType.RED, hit_point=24, items=[],
+				owner=UserType(
+					id='12345',
+					name='name',
+					group=ColorType.RED,
+					exp_point=0,
+					level=1,
+					created_at=datetime.now(),
+					updated_at=datetime.now()
+				),
+			),
+			SignType(
+				id='3333333333333333',
+				base_sign_types=[1],
+				owner=UserType(
+					id='12345',
+					name='name',
+					group=ColorType.RED,
+					exp_point=0,
+					level=1,
+					created_at=datetime.now(),
+					updated_at=datetime.now()
+				),
+				latitude=33.88223979967025,
+				longitude=130.87878426594528,
 				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
 				max_hit_point=100,
 				max_item_slot=8,
@@ -37,10 +77,10 @@ def get_map_info() -> MapInfo:
 				group=ColorType.RED, hit_point=24, items=[]
 			),
 			SignType(
-				id='3333333333333333',
+				id='3333333333333344',
 				base_sign_types=[1],
-				longitude=130.671892,
-				latitude=33.654921,
+				latitude=33.88204880963038,
+				longitude=130.8784008377654,
 				image_path='https://s3.ap-northeast-1.wasabisys.com/mastodondb/accounts/avatars/000/000/004/original/ed26601233e5b5cf.png',
 				max_hit_point=100,
 				max_item_slot=8,
