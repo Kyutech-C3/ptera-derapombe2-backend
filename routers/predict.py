ec8601f327f585111850324cddd0a5ebce0159c7
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-09-21 23:13:17 +0900

    [change] 推論をURLから可能にした

--- routers/predict.py
+++ routers/predict.py
@@ -5,16 +5,18 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 from strawberry.file_uploads import Upload
 from functions.predict import predict as predict_function
+import requests
+import numpy as np
 
-def predict(file: Upload) -> PredictResult:
-	try:
-		suffix = Path(file.filename).suffix
-		with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-			shutil.copyfileobj(file.file, tmp)
-		tmp_path = Path(tmp.name)
-	finally:
-		file.file.close()
-	print(tmp_path)
-	frame = cv2.imread(str(tmp_path))
+def predict(file: str) -> PredictResult:
+	# try:
+	# 	suffix = Path(file.filename).suffix
+	# 	with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+	# 		shutil.copyfileobj(file.file, tmp)
+	# 	tmp_path = Path(tmp.name)
+	# finally:
+	# 	file.file.close()
+	# print(tmp_path)
+	frame = cv2.imdecode(np.array(bytearray(requests.get(file).content), dtype=np.uint8), -1)
 
-	return predict_function(frame, tmp_path)
+	return predict_function(frame, file)
