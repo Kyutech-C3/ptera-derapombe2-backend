725e3b38798bbc7a65b25a9dc78f6d4b9a0830ea
PigeonsHouse <bullbull1230@gmail.com>
2022-09-21 20:45:56 +0900

    [fix] power_ratio

--- cruds/polygons.py
+++ cruds/polygons.py
@@ -4,10 +4,14 @@ from db.models import Color, Polygon
 from schemas.polygons import PowerRatio
 
 def calc_power_ratio(db: Session):
-	red_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.RED).group_by(Polygon.group)
-	green_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.GREEN).group_by(Polygon.group)
+	red_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.RED).group_by(Polygon.group).first()
+	if red_surface is None:
+		red_surface = 0
+	green_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.GREEN).group_by(Polygon.group).first()
+	if green_surface is None:
+		green_surface = 0
 
 	return PowerRatio(
-		red=red_surface/(red_surface+green_surface),
-		green=green_surface/(red_surface+green_surface)
+		red=red_surface/(red_surface+green_surface) if (red_surface+green_surface) > 0 else 0.5,
+		green=green_surface/(red_surface+green_surface) if (red_surface+green_surface) > 0 else 0.5
 	)
