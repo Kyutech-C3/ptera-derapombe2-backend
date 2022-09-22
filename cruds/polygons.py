75d05e90a0f5602f84dec228741b4c495b94cd46
PigeonsHouse <bullbull1230@gmail.com>
2022-09-22 13:14:31 +0900

    [fix]

--- cruds/polygons.py
+++ cruds/polygons.py
@@ -19,15 +19,13 @@ LENGTH_DEGREE = LENGTH_KILO_MATER * 90 / 10000
 
 def calc_power_ratio(db: Session):
 	red_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.RED).group_by(Polygon.group).first()
-	if red_surface is None:
-		red_surface = 0
+	red = red_surface[0] if red_surface is not None else 0
 	green_surface = db.query(func.sum(Polygon.surface)).filter(Polygon.group == Color.GREEN).group_by(Polygon.group).first()
-	if green_surface is None:
-		green_surface = 0
+	green = green_surface[0] if green_surface is not None else 0
 
 	return PowerRatio(
-		red=red_surface/(red_surface+green_surface) if (red_surface+green_surface) > 0 else 0.5,
-		green=green_surface/(red_surface+green_surface) if (red_surface+green_surface) > 0 else 0.5
+		red=red/(red+green) if (red+green) > 0 else 0.5,
+		green=green/(red+green) if (red+green) > 0 else 0.5
 	)
 
 
